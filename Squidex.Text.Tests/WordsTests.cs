a1ecccae59d46418c4a59010b49613a824149a7f
Sebastian <sebastian@squidex.io>
2020-08-27 20:51:44 +0200

    First commit.

---
+++ Squidex.Text.Tests/WordsTests.cs
@@ -0,0 +1,62 @@
+﻿// ==========================================================================
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex UG (haftungsbeschraenkt)
+//  All rights reserved. Licensed under the MIT license.
+// ==========================================================================
+
+using Xunit;
+
+namespace Squidex.Text.Tests
+{
+    public class WordsTests
+    {
+        [Fact]
+        public void Should_count_zero_words_for_empty_text()
+        {
+            Assert.Equal(0, Words.Count(string.Empty));
+        }
+
+        [Fact]
+        public void Should_count_zero_words_for_whitspace_text()
+        {
+            Assert.Equal(0, Words.Count(string.Empty));
+        }
+
+        [Fact]
+        public void Should_count_english_words()
+        {
+            Assert.Equal(4, Words.Count("You can't do that"));
+        }
+
+        [Fact]
+        public void Should_count_english_words_with_punctuation()
+        {
+            Assert.Equal(5, Words.Count("You can't do that, Mister."));
+        }
+
+        [Fact]
+        public void Should_count_english_words_with_extra_whitespaces()
+        {
+            Assert.Equal(4, Words.Count("You can't do  that. "));
+        }
+
+        [Fact]
+        public void Should_count_cjk_and_english_words()
+        {
+            Assert.Equal(7, Words.Count("Makes probably no sense: 空手道"));
+        }
+
+        [Fact]
+        public void Should_count_cjk_words()
+        {
+            Assert.Equal(3, Words.Count("空手道"));
+        }
+
+        [Fact]
+        public void Should_count_katakana_words()
+        {
+            Assert.Equal(3, Words.Count("カラテ カラテ カラテ"));
+        }
+    }
+}
---
+++ Squidex.Text/Words.cs
@@ -0,0 +1,63 @@
+﻿// ==========================================================================
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex UG (haftungsbeschraenkt)
+//  All rights reserved. Licensed under the MIT license.
+// ==========================================================================
+
+using System;
+
+namespace Squidex.Text
+{
+    /// <summary>
+    /// Helper methods to deal with words.
+    /// </summary>
+    public static class Words
+    {
+        /// <summary>
+        /// Counts the number of words in a text.
+        /// </summary>
+        /// <param name="text">The given text.</param>
+        /// <returns>
+        /// The number of found words in the text.
+        /// </returns>
+        public static int Count(string text)
+        {
+            if (text == null)
+            {
+                return 0;
+            }
+
+            return Count(text.AsSpan());
+        }
+
+        /// <summary>
+        /// Counts the number of words in a text.
+        /// </summary>
+        /// <param name="text">The given text.</param>
+        /// <returns>
+        /// The number of found words in the text.
+        /// </returns>
+        public static int Count(ReadOnlySpan<char> text)
+        {
+            if (text.Length == 0)
+            {
+                return 0;
+            }
+
+            var result = 0;
+
+            for (var i = 0; i < text.Length; i++)
+            {
+                var current = text[i];
+
+                if (!char.IsWhiteSpace(current) && !char.IsPunctuation(current) && WordBoundary.IsBoundary(text, i))
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
